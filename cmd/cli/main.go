c392252b80eae14f83e65249abbdd036df1c71b9
Vsevolod Kaloshin <vsevolod.kaloshin@iqoption.com>
2019-05-17 18:58:09 +0300

    main: added flags

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"flag"
 	"github.com/arzonus/sitemap/pkg/sitemap"
 	"log"
+	"os"
+	"runtime"
 	"time"
 )
 
+var (
+	parallel       = flag.Int("parallel", runtime.NumCPU(), "count of parallel requests to sites")
+	outputFilePath = flag.String("output-file", "sitemap.out", "filepath to output file")
+	maxDepth       = flag.Int("max-depth", 5, "max depth of handling sites")
+	timeout        = flag.Int("timeout", 30, "global timeout")
+)
+
 func main() {
+	flag.Parse()
+	if os.Args[1] == "" {
+		log.Print("url is empty")
+		os.Exit(1)
+	}
+
 	if err := sitemap.NewWalker(
-		"https://vk.com",
-		sitemap.MaxDepthOption(10),
-		sitemap.TimeoutOption(30*time.Second),
+		os.Args[1],
+		sitemap.MaxDepthOption(*maxDepth),
+		sitemap.WorkerCountOption(*parallel),
+		sitemap.TimeoutOption(time.Duration(*timeout)*time.Second),
 	).Run(); err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		os.Exit(1)
 	}
 }
